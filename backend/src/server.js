cbaf8da17de120c72713f303c4525af5b4cc35f8
youchen0716 <b06901181@ntu.edu.tw>
2021-06-29 20:38:13 +0800

    deploy at 6/29

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,6 +12,7 @@ const http = require('http');
 const WebSocket = require('ws');
 
 var app = express()
+var cors = require('cors')
 
 const db = mongoose.connection
 mongoose.connect(process.env.MONGO_URL, {
@@ -66,8 +67,13 @@ const wss = new WebSocket.Server({
   server,
 });
 var clients=[]
-app.use(express.static(path.join(__dirname, '..', 'public')));
+// app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.use(cors()) 
+app.get('/', (req, res) =>{
+  res.sendFile(path.join(__dirname, "..", "..", 'frontend', 'build', 'index.html'));
+});
+app.use(express.static(path.join(__dirname, "..", "..", 'frontend', 'build')));
 
 wss.on('connection', function connection(client){
   console.log('wss connected')
@@ -201,6 +207,6 @@ wss.on('connection', function connection(client){
   })
 })
 
-server.listen(4000, () => {
-  console.log('Server listening at http://localhost:4000');
+server.listen(process.env.PORT, () => {
+  console.log(`Server listening at http://localhost:${process.env.PORT}`);
 });
